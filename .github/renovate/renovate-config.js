37215eb9bac8c052fd6e48459149271342c97485
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2021-04-11 10:23:04 +0100

    create config file

--- .github/renovate/renovate-config.js
+++ .github/renovate/renovate-config.js
@@ -3,14 +3,32 @@
 const branchName = 'github-renovate';
 
 const fs = require('fs');
-const FOLDER = process.env.RENOVATE_CONFIG_FOLDER
+
+const config_folder = '.github/renovate'
+
 console.log(process.env);
 
-if (!fs.existsSync(`${FOLDER}/repositories.json`)) {
-  const err = `missing "${FOLDER}/repositories.json" file`
+fetch('https://github.com/ivankatliarchuk/.github/blob/main/.github/renovate/repositories.json')
+  .then(response => response.text())
+  .then(data => {
+    // Do something with your data
+    console.log(data);
+  });
+
+if (!fs.existsSync(`${config_folder}/repositories.json`)) {
+  const err = `missing "${config_folder}/repositories.json" file`
   console.log(`error: ${err}. exit...`)
   throw Error(err)
 }
+
+if (!fs.existsSync(`${config_folder}/config.json`)) {
+  const err = `missing "${config_folder}/config.json" file`
+  console.log(`error: ${err}. exit...`)
+  throw Error(err)
+}
+
+const cfg = JSON.parse(fs.readFileSync(`${config_folder}/config.json`), 'utf8');
+console.log(cfg);
 
 module.exports = {
   extends: [
@@ -18,7 +36,9 @@ module.exports = {
     "github>renovatebot/.github",
     ":disableRateLimiting"
   ],
+  assignees: process.env.LOG_LEVEL,
   logLevel: process.env.LOG_LEVEL,
+  labels: ['renovate', 'dependencies', 'automated'],
   branchPrefix: `${branchName}/`,
   dependencyDashboardTitle: 'Dependency Dashboard self-hosted',
   gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
@@ -28,5 +48,5 @@ module.exports = {
   dryRun: false,
   printConfig: true,
   username: process.env.USER_NAME,
-  repositories: JSON.parse(fs.readFileSync(`${FOLDER}/repositories.json`), 'utf8'),
+  repositories: JSON.parse(fs.readFileSync(`${config_folder}/repositories.json`), 'utf8'),
 };
